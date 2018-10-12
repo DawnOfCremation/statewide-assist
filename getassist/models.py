e16355a07de261ba038c54ed2a9ec213d54a1094
DawnOfCremation <damians@gmail.com>
2018-10-13 01:23:12 +1030

    first commit

--- getassist/models.py
+++ getassist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Getassist(models.Model):
+    myrego = models.CharField(max_length=10)
+    pub_date = models.DateTimeField()
+    vehicleMake = models.CharField(max_length=100)
+    vehicleModel = models.CharField(max_length=100)
+    myName = models.CharField(max_length=100)
+    #myPhone = models.IntegerField()
+    myLocation = models.CharField(max_length=200)
+    mynotes = models.TextField()
--- statewidera/settings.py
+++ statewidera/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'getassist.apps.GetassistConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'),)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
