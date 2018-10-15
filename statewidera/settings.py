be2b0204abe5d8361ea7c0c84977b34300dfb60d
DawnOfCremation <damians@gmail.com>
2018-10-15 22:06:24 +1030

    settings static224

--- statewidera/settings.py
+++ statewidera/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'statewidera/static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, .'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/home/statewidera/statewide_project/statewide-assist/static/')
 STATIC_URL = '/static/'
 
 
