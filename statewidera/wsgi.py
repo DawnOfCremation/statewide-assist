5725b6038be0a6b2a4b9b3e37a67d4103f551f17
DawnOfCremation <damians@gmail.com>
2018-10-20 02:10:22 +1030

    update wsgi

--- statewidera/wsgi.py
+++ statewidera/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statewidera.settings.local_settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statewidera.settings")
 
 application = get_wsgi_application()
