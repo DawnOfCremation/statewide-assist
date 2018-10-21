cacf90bdfe32cbc7227be447419ba15bcd93d6e8
DawnOfCremation <damians@gmail.com>
2018-10-21 22:06:27 +1030

    all new home pages and a bundh of image updates

--- statewidera/views.py
+++ statewidera/views.py
@@ -5,6 +5,12 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {'hithere': 'This is me'})
 
+def home2(request):
+    return render(request, 'home2.html')
+
+def home3(request):
+    return render(request, 'home3.html')
+
 
 
 def about(request):
