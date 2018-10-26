e4fdd4dff680701dad9f645e6ef6d5d63d57572d
DawnOfCremation <damians@gmail.com>
2018-10-27 02:24:32 +1030

    2 pages designed and cleaned

--- statewidera/urls.py
+++ statewidera/urls.py
@@ -8,10 +8,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('home2/', views.home2, name='home2'),
-    path('home3/', views.home3, name='home3'),
     path('about/', views.about, name='about'),
-    path('callassistance/', views.assistance, name='callassistance'),
-
+    path('contact/', views.contact, name='contact'),
 
 ]
--- statewidera/views.py
+++ statewidera/views.py
@@ -5,17 +5,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {'hithere': 'This is me'})
 
-def home2(request):
-    return render(request, 'home2.html')
-
-def home3(request):
-    return render(request, 'home3.html')
-
-
 
 def about(request):
     return render(request, 'about.html', {'aboutme': 'Lets Count!'})
 
 
-def assistance(request):
-    return render(request, 'callassistance.html')
+def contact(request):
+        return render(request, 'contact.html')
